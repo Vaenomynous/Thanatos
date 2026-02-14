7d3d9e45341399862af1f57c67b9898f3357bb16
Vaenomynous <117595397+Vaenomynous@users.noreply.github.com>
2026-02-14 13:23:53 -0600

    Add Termux-specific configuration settings

---
+++ termux_config.py
@@ -0,0 +1,8 @@
+# Termux Configuration Settings
+
+# Set up environment variables
+export TERMUX_PKG_TEMP_DIR=$HOME/Temp
+export TERMUX_PKG_INSTALL_DIR=$HOME/.termux-packages
+
+# Configure package repositories
+TERMUX_REPO=\\"https://your-repo-address\"
